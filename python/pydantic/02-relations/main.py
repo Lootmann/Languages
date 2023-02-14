97e0399ab81a92426bb102dc2e5c41934b6f9a19
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-02-14 09:03:43 +0900

    python: pydantic - validation

--- python/pydantic/02-relations/main.py
+++ python/pydantic/02-relations/main.py
@@ -1,3 +1,5 @@
+from pydantic import ValidationError
+
 import post as post_schema
 import user as user_schema
 
@@ -14,11 +16,31 @@ def get_posts():
 def main():
     user_data = {
         "id": 1,
+        "age": 94,
         "name": "hoge",
-        "posts": get_posts(),
+        "posts": [],
     }
+
     user = user_schema.User(**user_data)
     print(user)
+
+    try:
+        user_data = {
+            "id": "hoge",
+            "age": -1200,
+            "name": "hoge",
+            "posts": [],
+        }
+        user_schema.User(**user_data)
+    except ValidationError as e:
+        print("\n>>> e.errors()")
+        print(e.errors())
+
+        print("\n>>> e.json()")
+        print(e.json())
+
+        print("\n>>> str(e)")
+        print(str(e))
 
 
 if __name__ == "__main__":
--- python/pydantic/02-relations/user.py
+++ python/pydantic/02-relations/user.py
@@ -1,16 +1,23 @@
 from typing import List
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 
 import post
 
 
 class UserBase(BaseModel):
     id: int
+    age: int
     name: str
 
     class Config:
         orm_mode = True
+
+    @validator("age")
+    def age_must_between_0_to_120(cls, age_):
+        if 0 <= age_ <= 120:
+            return age_
+        raise ValueError("Field.Age must between 0 to 120")
 
 
 class User(UserBase):
